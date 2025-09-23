c6dd689427234c3e45ab6feac3ec6e1e21d3c16a
Matheus Fernandes Gomes <107514685+MatheusFerGo@users.noreply.github.com>
2025-09-23 08:54:23 -0300

    Time: 17 ms (34.61%) | Memory: 18 MB (62.53%) - LeetSync

---
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.py
@@ -0,0 +1,13 @@
+class Solution:
+    def makeEqual(self, words):
+        n = len(words)
+        freq = [0] * 26
+
+        for word in words:
+            for c in word:
+                freq[ord(c) - ord('a')] += 1
+        
+        for count in freq:
+            if count % n != 0:
+                return False
+        return True
