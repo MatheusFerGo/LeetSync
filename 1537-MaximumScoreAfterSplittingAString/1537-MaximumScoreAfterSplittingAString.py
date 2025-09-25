edf07e6420e64414373927c994bf867007745be2
Matheus Fernandes Gomes <107514685+MatheusFerGo@users.noreply.github.com>
2025-09-25 08:27:20 -0300

    Create 1537-MaximumScoreAfterSplittingAString

---
+++ 1537-MaximumScoreAfterSplittingAString/1537-MaximumScoreAfterSplittingAString.py
@@ -0,0 +1,21 @@
+# Last updated: 25/09/2025, 08:27:18
+class Solution:
+    def maxScore(self, s: str) -> int:
+        # Conta o total de '1' na string inteira (vai ser o valor inicial do lado direito)
+        total_ones = s.count('1')
+        
+        max_score = 0
+        left_zeros = 0
+        right_ones = total_ones
+        
+        # Percorre até o penúltimo caractere (divisão deve deixar os dois lados não-vazios)
+        for i in range(len(s) - 1):
+            if s[i] == '0':
+                left_zeros += 1
+            else:
+                right_ones -= 1
+            
+            score = left_zeros + right_ones
+            max_score = max(max_score, score)
+        
+        return max_score
