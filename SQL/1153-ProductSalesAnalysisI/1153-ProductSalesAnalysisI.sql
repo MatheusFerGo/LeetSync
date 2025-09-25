7f8df6645c29a709b60ccbca53c2a9d40a653bd9
Matheus Fernandes <matheus.fernangomes@gmail.com>
2025-09-25 10:45:14 -0300

    ajustes de extensão

--- SQL/1153-ProductSalesAnalysisI/1153-ProductSalesAnalysisI.sql
+++
@@ -1,3 +0,0 @@
--- Last updated: 25/09/2025, 08:27:17
-SELECT p.product_name, s.year, s.price FROM Sales as s, Product as p
-WHERE p.product_id = s.product_id
--- SQL/1509-ReplaceEmployeeIdWithTheUniqueIdentifier/1509-ReplaceEmployeeIdWithTheUniqueIdentifier.sql
+++
@@ -1,9 +0,0 @@
--- Last updated: 25/09/2025, 08:27:14
-# Write your MySQL query statement below
-SELECT
-    eu.unique_id,
-    e.name
-FROM
-    Employees AS e
-LEFT JOIN
-    EmployeeUNI AS eu ON e.id = eu.id;
