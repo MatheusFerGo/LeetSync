526a3ec6f644c9b71c6d0c9d58d39263e5f7c0ac
Matheus Fernandes Gomes <107514685+MatheusFerGo@users.noreply.github.com>
2026-03-21 05:31:57 -0300

    Time: 8 ms (41.30%) | Memory: 44.9 MB (21.78%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+    if (s.Length != t.Length) return false;
+
+    var counts = new Dictionary<char, int>();
+
+    foreach (char c in s) {
+        counts[c] = counts.GetValueOrDefault(c, 0) + 1;
+    }
+
+    foreach (char c in t) {
+        if (!counts.ContainsKey(c) || counts[c] == 0) return false;
+        counts[c]--;
+    }
+
+    return true;
+    }
+}
