bfee9affa42f385824f311f20c0d1159576988bb
MatheusFerGo <107514685+MatheusFerGo@users.noreply.github.com>
2026-07-03 22:28:41 +0000

    chore(auto): Processa novas soluções e atualiza o README

--- Python/Easy/21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ Python/Easy/21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,23 +1,21 @@
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(-1)
-        current = dummy
+        no_falso_ancora = ListNode(val=-1)
+        ponteiro_atual = no_falso_ancora
 
-        while list1 and list2:
-            if list1.val < list2.val:
-                current.next = list1
+        while list1 is not None and list2 is not None:
+            if list1.val <= list2.val:
+                ponteiro_atual.next = list1
                 list1 = list1.next
             else:
-                current.next = list2
+                ponteiro_atual.next = list2
                 list2 = list2.next
+            
+            ponteiro_atual = ponteiro_atual.next
 
-            current = current.next
+        if list1 is not None:
+            ponteiro_atual.next = list1
+        else:
+            ponteiro_atual.next = list2
         
-        current.next = list1 if list1 else list2
-        
-        return dummy.next
+        return no_falso_ancora.next
--- README.md
+++ README.md
@@ -46,11 +46,11 @@ Este repositório contém minhas soluções para diversos problemas da plataform
 - [242 Valid Anagram `(Easy)`](https://leetcode.com/problems/valid-anagram)
 
 ### 💻 CSharp
+- [21 Merge Two Sorted Lists `(Easy)`](https://leetcode.com/problems/merge-two-sorted-lists)
 - [908 Middle Of The Linked List `(Easy)`](https://leetcode.com/problems/middle-of-the-linked-list)
 - [206 Reverse Linked List `(Easy)`](https://leetcode.com/problems/reverse-linked-list)
 - [104 Maximum Depth Of Binary Tree `(Easy)`](https://leetcode.com/problems/maximum-depth-of-binary-tree)
 - [49 Group Anagrams `(Medium)`](https://leetcode.com/problems/group-anagrams)
-- [242 Valid Anagram `(Easy)`](https://leetcode.com/problems/valid-anagram)
 
 ### 🗄️ SQL
 - [1136 Actors And Directors Who Cooperated At Least Three Times `(Easy)`](https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times)
